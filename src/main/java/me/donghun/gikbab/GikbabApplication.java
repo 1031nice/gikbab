fd52345cf240f323b0cd25ff7772f3df1ec42c5b
dong-hun <1031nice@gmail.com>
2020-06-07 22:54:28 +0900

    Project 'gikbab' first commit

---
+++ src/main/java/me/donghun/gikbab/GikbabApplication.java
@@ -0,0 +1,13 @@
+package me.donghun.gikbab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GikbabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GikbabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/donghun/gikbab/GikbabApplicationTests.java
@@ -0,0 +1,13 @@
+package me.donghun.gikbab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GikbabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

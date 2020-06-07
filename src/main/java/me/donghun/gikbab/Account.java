74e4d812d28c79ae5873910343dd71e160bb3746
dong-hun <1031nice@gmail.com>
2020-06-07 22:58:53 +0900

    Make 'Account' class

---
+++ src/main/java/me/donghun/gikbab/Account.java
@@ -0,0 +1,30 @@
+package me.donghun.gikbab;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Account {
+
+    // 한 사람마다 '취향'을 나타내는 객체를 가지고 있게 하는건 어떨까
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/me/donghun/gikbab/GikbabApplication.java
+++ src/main/java/me/donghun/gikbab/GikbabApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GikbabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GikbabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GikbabApplication.class, args);
+    }
 
 }

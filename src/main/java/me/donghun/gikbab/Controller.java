644b6ca77e5e66de678ee1ef30ddebdba367c97e
Dong-Hun Gang <1031nice@gmail.com>
2020-06-25 23:48:42 +0900

    day diet -> breakfast / lunch / dinner

--- src/main/java/me/donghun/gikbab/Controller.java
+++ src/main/java/me/donghun/gikbab/Controller.java
@@ -19,11 +19,14 @@ import java.util.Date;
 public class Controller {
 
     @ResponseBody
-    @GetMapping("/menu")
-    public String getMenu() throws IOException {
+    @GetMapping("/todayDiet")
+    public String todayDiet() throws IOException {
+
+        // 오늘 날짜 계산
         Date date = new Date();
         String[] splitDate = date.toString().split(" ");
         int day = Integer.parseInt(Arrays.asList(splitDate).get(2));
+
         URL url = new URL("http://dorm.cnu.ac.kr/html/kr/sub03/sub03_0304.html");
         URLConnection con = url.openConnection();
         InputStream is = con.getInputStream();
@@ -31,6 +34,8 @@ public class Controller {
         String line = null;
         StringBuilder builder = new StringBuilder();
         boolean flag = false;
+
+        // 1차 가공: 원하는 요일의 식사 정보 가져오기
         while ((line = br.readLine()) != null) {
             if(line.contains(day + "(")){ // 원하는 요일 찾음
                 flag = true;
@@ -47,7 +52,36 @@ public class Controller {
             if(flag) // 원하는 요일을 찾고 저장까지 끝냈으므로 반복 탈출
                 break;
         }
-        return builder.toString();
+        String naive = builder.toString();
+
+        // 2차 가공: 아침 점심 저녁 구분하기
+        int breakfast = -1, lunch = -1, dinner = -1, count = 0;
+        String replace = naive.replace("<br />", "");
+        for(int i=0; i<replace.length(); i++) {
+            if(replace.charAt(i) == 'A'){
+                count++;
+                switch (count) {
+                    case 1:
+                        breakfast = i;
+                        break;
+                    case 3:
+                        lunch = i;
+                        break;
+                    case 5:
+                        dinner = i;
+                        break;
+                }
+            }
+        }
+        String s1 = replace.substring(0, breakfast);
+        String s2 = replace.substring(breakfast, lunch);
+        String s3 = replace.substring(lunch, dinner);
+        String s4 = replace.substring(dinner, replace.length());
+        String str = s1 + "아침<br />" + s2 + "점심<br />" + s3 + "저녁<br />" + s4;
+        String[] split = str.split("메인");
+        StringBuilder builder2 = new StringBuilder();
+        Arrays.stream(split).forEach(s -> builder2.append(s+"<br />"));
+        return builder2.toString();
     }
 
 }
